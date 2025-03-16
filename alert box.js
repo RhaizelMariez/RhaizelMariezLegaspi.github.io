34e24b3b8e38efc3284491726bebc6201f287b21
Rhaizel <rhaizellegaspi@gmail.com>
2025-03-16 21:50:38 +0800

    Add files via upload

---
+++ alert box.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head><title> ICT </title> 
+<script type="text/javascript">
+function show_alert()
+{alert ("Good day! This is my e-portfolio for ICT!");}
+</script>
+</head>
+<body>
+<input type="button" onclick="show_alert()" value="show alert box"/>
+</body>
+</html>
